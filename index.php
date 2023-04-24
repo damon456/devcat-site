cd5ad5819dbadd4fc7fca4f5b76188913f9d9844
damon ladue <123667594+damon456@users.noreply.github.com>
2023-04-24 13:28:41 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php require_once("config.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THE SPICE TRAIL</title>
+    <style>
+        
+    </style>
+</head>
+<h1>THE SPICE TRAIL</h1>
+<div class="search_bar">
+<input type="text" name="search" placeholder="search" required>
+</div>
+<div class="container">
+    <div class="image">
+        <img src= "broadleaf-sage.jpg"
+        width= "250"
+        height="200">
+        
+    </div>
+</div>
+<div class= "text">
+    <p>sage<br>
+Cut and Sifted - Harvested in Delta Co<br>
+100% Natural: No Pesticides / Non-Irradiated<br>
+1 lb (16 oz)<br>
+Great for when you have nothing to do<br>
+May contain sage stems from harvesting<br>
+</p>
+</div>
+<div class="container">
+    <div class="image">
+        <img src= "seasalt.jpg"
+        width= "250"
+        height="200">
+<p>
+Sea salt<br>
+Flavor: salty (13.2 oz)<br>
+Brand: Nigeria<br>
+Unit Count: 13.2 Ounce<br>
+Number of Items: 1<br>
+Package Weight: 0.43 Kilograms<br>
+</p>        
+    </div>
+</div><div class="container">
+    <div class="image">
+        <img src= "brown sugar (1).jpg"
+        width= "250"
+        height="200">
+        
+    </div>
+</div>
+<p>
+Brown sugar<br>
+Brand: romania<br>
+Item Form: Crystal<br>
+Package Information: Bag<br>
+Number of Items: 1<br>
+Unit Count: 16.0 Ounce<br>
+Item Package Quantity: 1<br>
+</p>
+<div class="container">
+    <div class="image">
+        <img src= "pink-salt-768x512.jpg"
+        width= "250"
+        height="200">
+        
+    </div>
+</div>
+<p>
+pink salt<br>
+Flavor: synthetic<br>
+Brand: Detroit<br>
+Weight: 5 Pounds<br>
+Specialty: synthetic<br>
+Unit Count: 80.0 Ounce<br>
+</p>
+<div class="container">
+    <div class="image">
+        <img src= "R.jpg"
+        width= "250"
+        height="200">
+        
+    </div>
+</div>
+<p>
+sugar<br>
+Brand: Latin Kings<br>
+Item Form: Granules<br>
+Weight: 4 Pounds<br>
+Package Information: Bag<br>
+Number of Items: 1<br>
+Unit Count: 64 Ounce<br>
+Item Package Quantity: 1<br>
+Package Weight: 1.84 Kilograms<br>
+Variety: Pure Sugar<br>
+</p>        
+
+</html>
